b1510c6d8a2b55ac0cdd36779de63e9caf949788
Leach <anna.leach@FT-MW4802.local>
2017-03-24 16:04:34 +0000

    Fix mouse move events

--- config/article.js
+++ config/article.js
@@ -16,7 +16,7 @@ export default () => ({ // eslint-disable-line
   // summary === standfirst (Summary is what the content API calls it)
   summary: 'Saudi State Oil company is likely to be the biggest company in the world when it goes to IPO next year.' +
   'Play with the factors that could push it to be the globe\'s first trillion dollar company or shrink it to middling tens of billions.' + 
-  'Choose a scenario or or fine tune two key variables, oil price and tax rates to determine just how large it could be.'
+  'Choose a scenario or or fine tune two key variables, oil price and tax rates to determine just how large it could be.',
 
   topic: {
     name: 'Saudi Aramco IPO',
